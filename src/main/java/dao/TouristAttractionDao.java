2b484e64b6b36f5b40086c30ea02c85a5f9c7bcd
freeDaTitty <danieldinu6091@gmail.com>
2022-05-25 17:55:08 +0300

    Refactored TouristAttractionDao

--- src/main/java/dao/TouristAttractionDao.java
+++ src/main/java/dao/TouristAttractionDao.java
@@ -1,17 +1,13 @@
 package dao;
 
 import model.Hotel;
+import model.Restaurant;
 import model.TouristAttraction;
 
 import java.util.List;
 
 public interface TouristAttractionDao {
     void addTouristAttraction(TouristAttraction touristAttraction);
-    List<TouristAttraction> getAll();
-
     void removeTouristAttraction(TouristAttraction touristAttraction);
-
-
-
-
+    List<TouristAttraction> getAllTouristAttractions();
 }
