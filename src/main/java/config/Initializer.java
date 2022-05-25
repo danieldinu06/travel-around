23cde2636f22bf950c373d5a898d910e7b66e2a9
Cristina Mincov <virmincov21@yahoo.com>
2022-05-25 18:04:44 +0300

    added configuration

---
+++ src/main/java/config/Initializer.java
@@ -0,0 +1,15 @@
+package config;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+
+@WebListener
+public class Initializer implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+    }
+}
