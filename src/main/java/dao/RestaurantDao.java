106f544e4ab0e489128715d511e3ed871c5078dd
freeDaTitty <danieldinu6091@gmail.com>
2022-05-25 17:55:38 +0300

    Refactored RestaurantDao

---
+++ src/main/java/dao/RestaurantDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import model.Restaurant;
+
+import java.util.List;
+
+public interface RestaurantDao {
+    void addRestaurant(Restaurant restaurant);
+    Restaurant getRestaurant(String name);
+    List<Restaurant> getRestaurants();
+}
