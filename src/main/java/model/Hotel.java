204e091b3e9eb2d44b2617d95de91dcdbacbebbe
Cristina Mincov <virmincov21@yahoo.com>
2022-05-25 17:02:37 +0300

    small changes in the hotel class

--- src/main/java/model/Hotel.java
+++ src/main/java/model/Hotel.java
@@ -24,16 +24,8 @@ public class Hotel {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getImage() {
         return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
     }
 
     public String getDescription() {
