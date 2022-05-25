3d2784804513e750f20688473ffe84b19249a475
Cristina Mincov <virmincov21@yahoo.com>
2022-05-25 23:08:19 +0300

    partially implemented HotelDaoJDBC

--- src/main/java/dao/implementation/HotelDao.java
+++ src/main/java/dao/implementation/HotelDao.java
@@ -1,4 +1,4 @@
-package dao;
+package dao.implementation;
 
 import model.Hotel;
 
---
+++ src/main/java/dao/jdbc/HotelDaoJDBC.java
@@ -0,0 +1,37 @@
+package dao.jdbc;
+
+import dao.implementation.HotelDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+
+public class HotelDaoJDBC implements HotelDao {
+    private static final Logger logger = LoggerFactory.getLogger(HotelDaoJDBC.class);
+    private final DataSource dataSource;
+    private static HotelDao instance;
+
+    private HotelDaoJDBC(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static HotelDao getInstance(DataSource dataSource) {
+        if (instance == null) {
+            instance = new HotelDaoJDBC(dataSource);
+        }
+        return instance;
+    }
+
+    public static HotelDao getInstance() {
+        return instance;
+    }
+
+//    @Override
+//    public void add(Product product) {
+//        try (Connection conn = dataSource.getConnection()) {
+//            String sql = "INSERT INTO ";
+//
+//        } catch (SQLException e) {
+//            logger.error("Error while adding product: '{}'", product.toString());
+//        }
+//    }
