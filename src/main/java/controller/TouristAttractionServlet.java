9bb4e914f3bbbd9439fec645f02517ab062be8e1
freeDaTitty <danieldinu6091@gmail.com>
2022-05-26 17:08:35 +0300

    Refactored TouristAttractionServlet as it should be!

--- src/main/java/controller/TouristAttractionServlet.java
+++ src/main/java/controller/TouristAttractionServlet.java
@@ -1,5 +1,9 @@
 package controller;
 
+import config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,66 +32,18 @@ public class TouristAttractionServlet extends HttpServlet {
         //Assignee - Razvan
 
 
+        TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext webContext = new WebContext(req, resp, req.getServletContext());
 
+        /*
+        * Aici vom seta variabilele contextului nostru de web.
+        * Adica tourisitAttraction in felul asta:
+        *
+        *   webContext.setVariable("touristAttractions, ceva);
+        *
+        *   Acel CEVA de mai sus va fi un obiect de tip TouristAttractionDaoJDBC asupra caruia vom folosi functia getAll.
+        */
 
-        printWriter.println(
-                "\"<html>\\n\" +\n" +
-                        "                        \"<head>\" +\n" +
-                        "                        \"  <title>\" + title + \"</title>\" +\n" +
-                        "                        \"  <link rel=\\\"stylesheet\\\" type=\\\"text/css\\\" href='/static/css/site.css' />\" +\n" +
-                        "                        \"  <title>Hacker com.codecool.hackernews.models.News</title>\\n\" +\n" +
-                        "                        \"<link rel=\\\"stylesheet\\\" href=\\\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\\\">\\n\" +\n" +
-                        "                        \"<script src=\\\"https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js\\\"></script>\\n\" +\n" +
-                        "                        \"<script src=\\\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\\\"></script>\\n\" +\n" +
-                        "                        \"<script src=\\\"static/js/main.js\\\" defer></script>\\n\" +\n" +
-                        "                        \"</head>\" +\n" +
-                        "                        \"</head>\\n\" +\n" +
-                        "                        \"<body>\\n\" +\n" +
-                        "                        \"<nav class=\\\"navbar navbar-inverse\\\">\\n\" +\n" +
-                        "                        \"    <div class=\\\"container-fluid\\\">\\n\" +\n" +
-                        "                        \"        <div class=\\\"navbar-header\\\">\\n\" +\n" +
-                        "                        \"            <a class=\\\"navbar-brand\\\" href=\\\"/\\\">\" + title + \"</a>\\n\" +\n" +
-                        "                        \"        </div>\\n\" +\n" +
-                        "                        \"        <ul class=\\\"nav navbar-nav\\\">\\n\" +\n" +
-                        "                        \"            <li><a href=\\\"/\\\">\" + hacksonNews + \"</a></li>\\n\" +\n" +
-                        "                        \"            <li><a href=\\\"/topNews\\\">\" + topNews + \"</a></li>\\n\" +\n" +
-                        "                        \"            <li><a href=\\\"/newest\\\">\"+ newest +\"</a></li>\\n\" +\n" +
-                        "                        \"            <li><a href=\\\"/jobs\\\">\"+ jobs +\"</a></li>\\n\" +\n" +
-                        "                        \"        </ul>\\n\" +\n" +
-                        "                        \"    </div>\\n\" +\n" +
-                        "                        \"</nav>\" +\n" +
-                        "                        \"<h1 align = \\\"center\\\">\" + hacksonNews + \"</h1>\\n\" +\n" +
-                        "                        \"<nav aria-label=\\\"Page navigation example\\\" class=\\\"pagination\\\">\\n\" +\n" +
-                        "                        \"  <ul class=\\\"pagination\\\">\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\"> << </a></li>\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\">1</a></li>\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\">2</a></li>\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\">3</a></li>\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\">4</a></li>\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\">5</a></li>\\n\" +\n" +
-                        "                        \"    <li class=\\\"page-item\\\"><a class=\\\"page-link\\\" href=\\\"#\\\"> >> </a></li>\\n\" +\n" +
-                        "                        \"  </ul>\\n\" +\n" +
-                        "                        \"</nav>\" +\n" +
-                        "                        \"<div id=\\\"items\\\">\" +\n" +
-                        "                        \"</div>\" +\n" +
-                        "                        \"<ul>\\n\" +\n" +
-                        "                        \" <footer>\\n\" +\n" +
-                        "                        \"  <!-- Copyright -->\\n\" +\n" +
-                        "                        \" <div>\\n\" +\n" +
-                        "                        \"© 2020 Copyright:\\n\" +\n" +
-                        "                        \"<a href=\\\"https://www.linkedin.com/in/razvan-daniel-besleaga-52466910b/\\\" target=\\\"_blank\\\">Razvan-Daniel Besleaga</a>\\n\" +\n" +
-                        "                        \"<p id=\\\"email\\\"><a href=\\\"mailto:razvandallas09@gmail.com\\\" target=\\\"_blank\\\">Email Address</a></p>\\n\" +\n" +
-                        "                        \"</div>\\n\" +\n" +
-                        "                        \"<!-- Copyright -->\\n\" +\n" +
-                        "                        \"</footer> \" +\n" +
-                        "                        \"</body></html>\""
-
-
-
-
-
-
-        );
-
+        templateEngine.process("index.html", webContext);
     }
 }
