46eaf041cee07b239ff151f506f2d702a777b991
freeDaTitty <danieldinu6091@gmail.com>
2022-05-26 14:20:51 +0300

    Refactored Restaurant clasa

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -1,12 +1,26 @@
 package model;
 
 public class Restaurant {
+    private Integer id;
     private final String name;
     private float rating;
+    private TouristAttraction touristAttraction;
 
     public Restaurant(String name, float rating) {
         this.name = name;
         this.rating = rating;
+    }
+
+    public TouristAttraction getTouristAttraction() {
+        return touristAttraction;
+    }
+
+    public void setTouristAttraction(TouristAttraction touristAttraction) {
+        this.touristAttraction = touristAttraction;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public String getName() {
@@ -15,6 +29,10 @@ public class Restaurant {
 
     public float getRating() {
         return rating;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public void setRating(float rating) {
