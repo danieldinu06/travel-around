d544430888dee22674b8d9ba45d9417a3a5d2ccb
freeDaTitty <danieldinu6091@gmail.com>
2022-05-26 14:19:38 +0300

    Refactored Initializer class

--- src/main/java/config/Initializer.java
+++ src/main/java/config/Initializer.java
@@ -3,13 +3,16 @@ package config;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.SQLException;
 
 
 @WebListener
 public class Initializer implements ServletContextListener {
+    ApplicationService applicationService;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        applicationService = new ApplicationService();
 
     }
 }
