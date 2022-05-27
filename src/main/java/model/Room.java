d7027ca94aac99ce5e8cf352636bc14cbde8444d
freeDaTitty <danieldinu6091@gmail.com>
2022-05-27 14:12:46 +0300

    Refactored Room class

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -4,11 +4,17 @@ public class Room {
     private final int beds;
     private final int floor;
     private float price;
+    private final Hotel hotel;
 
-    public Room(int beds, int floor, float price) {
+    public Room(int beds, int floor, float price, Hotel hotel) {
         this.beds = beds;
         this.floor = floor;
         this.price = price;
+        this.hotel = hotel;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
     }
 
     public int getBeds() {
