042176379a8017895a79b4f82742010865de8df7
freeDaTitty <danieldinu6091@gmail.com>
2022-05-28 20:42:25 +0300

    Implemented get Restaurant by Id

--- src/main/java/dao/jdbc/RestaurantDaoJDBC.java
+++ src/main/java/dao/jdbc/RestaurantDaoJDBC.java
@@ -1,6 +1,7 @@
 package dao.jdbc;
 
 import dao.RestaurantDao;
+import dao.TouristAttractionDao;
 import model.Restaurant;
 import model.TouristAttraction;
 
@@ -9,11 +10,13 @@ import java.sql.*;
 import java.util.List;
 
 public class RestaurantDaoJDBC implements RestaurantDao {
+    private TouristAttractionDao touristAttractionDao;
 
     private final DataSource dataSource;
 
-    public RestaurantDaoJDBC(DataSource dataSource) {
+    public RestaurantDaoJDBC(DataSource dataSource, TouristAttractionDao touristAttractionDao) {
         this.dataSource = dataSource;
+        this.touristAttractionDao = touristAttractionDao;
     }
 
     @Override
@@ -21,6 +24,7 @@ public class RestaurantDaoJDBC implements RestaurantDao {
         try (Connection connection = dataSource.getConnection()) {
             String sql = "INSERT INTO restaurants (name, t_attraction_id, rating) VALUES(?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
             statement.setString(1, restaurant.getName());
             statement.setInt(2, restaurant.getTouristAttraction().getId());
             statement.setFloat(3, restaurant.getRating());
@@ -37,8 +41,30 @@ public class RestaurantDaoJDBC implements RestaurantDao {
     }
 
     @Override
-    public Restaurant get(String name) {
-        return null;
+    public Restaurant get(Integer id) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT (name, t_attraction_id, rating) FROM restaurants WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()) return null;
+
+            int t_attraction_id = resultSet.getInt(2);
+            TouristAttraction touristAttraction = touristAttractionDao.get(t_attraction_id);
+
+            String name = resultSet.getString(1);
+            float rating = resultSet.getFloat(2);
+
+            Restaurant restaurant = new Restaurant(name, rating, touristAttraction);
+            restaurant.setId(id);
+
+            return restaurant;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while getting a Restaurant");
+        }
     }
 
     @Override
