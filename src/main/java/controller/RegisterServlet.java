da32b780676a90a5a25396ad2f6c21b6ce2e312f
freeDaTitty <danieldinu6091@gmail.com>
2022-05-31 11:04:06 +0300

    Added RegisterServlet

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -1,12 +1,20 @@
 package controller;
 
+import config.TemplateEngineUtil;
+import dao.UserDao;
 import model.User;
+import model.utils.Encrypt;
+import model.utils.UserStatus;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import service.ApplicationService;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/register"})
 public class RegisterServlet extends HttpServlet {
@@ -17,7 +25,24 @@ public class RegisterServlet extends HttpServlet {
         httpSession = request.getSession();
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        String name = request.getParameter("name");
+        String password = Encrypt.encode(request.getParameter("password"));
+        String phoneNumber = request.getParameter("phone_number");
+        String billingAddress = request.getParameter("billing_address");
+        UserStatus userStatus = UserStatus.valueOf(request.getParameter("user_status"));
+
+        user = new User(name, password, phoneNumber, billingAddress, userStatus);
+
+
+        httpSession = request.getSession(true);
         httpSession.setAttribute("user", user);
+
+        ApplicationService applicationService = ApplicationService.getInstance();
+        applicationService.userDao.add(user);
+
+        response.sendRedirect(request.getContextPath() + "/second");
     }
 }
