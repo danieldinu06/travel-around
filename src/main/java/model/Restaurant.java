9d4c331ad27142411fd48e870ce50a442e74edc6
freeDaTitty <danieldinu6091@gmail.com>
2022-06-05 20:35:03 +0300

    Initialized images in Restaurant class

--- src/main/java/model/Restaurant.java
+++ src/main/java/model/Restaurant.java
@@ -1,15 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
     private Integer id;
     private final String name;
     private float rating;
+    private List<String> images;
     private final TouristAttraction touristAttraction;
 
     public Restaurant(String name, float rating, TouristAttraction touristAttraction) {
         this.name = name;
         this.rating = rating;
         this.touristAttraction = touristAttraction;
+        this.images = new ArrayList<>();
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void addImage(String image) {
+        this.images.add(image);
     }
 
     public TouristAttraction getTouristAttraction() {
