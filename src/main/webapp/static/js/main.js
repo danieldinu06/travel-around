0d01f8c00c54d6f18d8c784c19d03cba52a21b2f
freeDaTitty <danieldinu6091@gmail.com>
2022-06-06 12:18:48 +0300

    Modified main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -3,17 +3,3 @@ import 'boxicons'
 $('.carousel').carousel({
     interval: 2500
 });
-
-let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=>{
-        let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-        arrowParent.classList.toggle("showMenu");
-    });
-}
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-menu");
-console.log(sidebarBtn);
-sidebarBtn.addEventListener("click", ()=>{
-    sidebar.classList.toggle("close");
-});
