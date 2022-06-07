f0dae66c2716911c0c7fc469a1bb8e33199d61c5
freeDaTitty <danieldinu6091@gmail.com>
2022-06-07 14:59:03 +0300

    Refactored TouristAttractionServlet

--- src/main/java/controller/TouristAttractionServlet.java
+++ src/main/java/controller/TouristAttractionServlet.java
@@ -2,6 +2,7 @@ package controller;
 
 import config.TemplateEngineUtil;
 import model.TouristAttraction;
+import model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import service.ApplicationService;
@@ -11,12 +12,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet(name = "touristAttractionServlet", urlPatterns = {"/"}, loadOnStartup = 1)
 public class TouristAttractionServlet extends HttpServlet {
+    HttpSession httpSession;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -25,16 +28,19 @@ public class TouristAttractionServlet extends HttpServlet {
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext webContext = new WebContext(req, resp, req.getServletContext());
+        TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext webContext = new WebContext(request, response, request.getServletContext());
 
         ApplicationService applicationService = ApplicationService.getInstance();
         List<TouristAttraction> touristAttractions = applicationService.touristAttractionDao.getAll();
 
+        httpSession = request.getSession(true);
+        User user = (User) httpSession.getAttribute("user");
+
         webContext.setVariable("touristAttractions", touristAttractions);
-        webContext.setVariable("username", "Daniel");
-        templateEngine.process("index.html", webContext, resp.getWriter());
+        webContext.setVariable("username", user.getName());
+        templateEngine.process("index.html", webContext, response.getWriter());
     }
 }
