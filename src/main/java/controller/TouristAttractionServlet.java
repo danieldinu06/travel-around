eb6438d69d95b211d1a9ecb03315cf0ce47a6ea4
freeDaTitty <danieldinu6091@gmail.com>
2022-06-07 20:28:12 +0300

    Implemented Servlet for a single Tourist Attraction

---
+++ src/main/java/controller/TouristAttractionServlet.java
@@ -0,0 +1,43 @@
+package controller;
+
+import config.TemplateEngineUtil;
+import model.TouristAttraction;
+import model.User;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import service.ApplicationService;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(urlPatterns = {"/id/*"})
+public class TouristAttractionServlet extends HttpServlet {
+    HttpSession httpSession;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext webContext = new WebContext(request, response, request.getServletContext());
+
+        ApplicationService applicationService = ApplicationService.getInstance();
+        Integer id = Integer.valueOf(request.getPathInfo().substring(1));
+        TouristAttraction touristAttraction = applicationService.touristAttractionDao.get(id);
+
+        httpSession = request.getSession(true);
+        User user = (User) httpSession.getAttribute("user");
+
+        if (user == null)
+            response.sendRedirect(request.getContextPath() + "/login");
+
+        webContext.setVariable("touristAttraction", touristAttraction);
+        if (user != null) {
+            webContext.setVariable("username", user.getName());
+        }
+        templateEngine.process("tourist_attraction.html", webContext, response.getWriter());
+    }
+}
