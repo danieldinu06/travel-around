a62bbf3d40bf3de2fb5a9e0274d44db6c8bcd437
freeDaTitty <danieldinu6091@gmail.com>
2022-06-08 19:35:49 +0300

    Initialized new Map from schedule in Tourist Attraction class

--- src/main/java/model/TouristAttraction.java
+++ src/main/java/model/TouristAttraction.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,6 +23,7 @@ public class TouristAttraction {
         this.rating = rating;
         this.hotels = new ArrayList<>();
         this.restaurants = new ArrayList<>();
+        this.schedule = new HashMap<>();
     }
 
     public Map<String, String> getSchedule() {
