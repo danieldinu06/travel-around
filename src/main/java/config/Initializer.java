f043d75c921eb548c83121cc8ac5b50b0706b5a3
freeDaTitty <danieldinu6091@gmail.com>
2022-06-08 17:27:26 +0300

    Refactored Initializer class

--- src/main/java/config/Initializer.java
+++ src/main/java/config/Initializer.java
@@ -32,15 +32,16 @@ public class Initializer implements ServletContextListener {
                 "\n" +
                 "It is the perfect setting for a vampire-lord. Tall, turreted, red-roofed, with a massive guard tower, it has thick walls with narrow openings for archers and cannons, iron grates over the portals, and secret passageways.\n" +
                 "\n" +
-                "The Bran castle had to be mended and refurbished over centuries of struggle with the Ottoman Empire. Once the two spacious inner courtyards had been flower gardens, and the white-washed walls of the high-ceilinged rooms had been covered with brilliant-hued carpets from the northern reaches of Moldavia.", (float)5.0);
+                "The Bran castle had to be mended and refurbished over centuries of struggle with the Ottoman Empire. Once the two spacious inner courtyards had been flower gardens, and the white-washed walls of the high-ceilinged rooms had been covered with brilliant-hued carpets from the northern reaches of Moldavia.", (float)4.4);
 
         branCastle.setUrl("http://www.castelulbran.ro");
+        branCastle.setLocation("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2795.7603746299565!2d25.364975015939425!3d45.51490217910141!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b347e5a415de31%3A0xcf922792d921ab7f!2sCastelul%20Bran!5e0!3m2!1sro!2sro!4v1654683774551!5m2!1sro!2sro");
 
-        Hotel conaculBratescu = new Hotel("Conacul Bratescu", "", (float)4.3, 30, branCastle);
+        Hotel conaculBratescu = new Hotel("Conacul Bratescu", "", (float)4.8, 30, branCastle);
         Hotel casaMedievala = new Hotel("Pensiunea Casa Medievala", "", (float)4.6, 25, branCastle);
 
-        Restaurant torzburg = new Restaurant("Restaurant Torzburg", (float)4.6, branCastle);
-        Restaurant trattoriaAlGallo = new Restaurant("Trattoria Al Gallo", (float)4.9, branCastle);
+        Restaurant torzburg = new Restaurant("Restaurant Torzburg", (float)4.4, branCastle);
+        Restaurant trattoriaAlGallo = new Restaurant("Trattoria Al Gallo", (float)4.0, branCastle);
 
         branCastle.addHotel(conaculBratescu);
         branCastle.addHotel(casaMedievala);
