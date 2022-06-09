6300e27585ea7ea36ae5facd4389829d9a826290
freeDaTitty <danieldinu6091@gmail.com>
2022-06-09 12:37:01 +0300

    Repaired rating js

--- src/main/webapp/static/js/rating.js
+++ src/main/webapp/static/js/rating.js
@@ -5,12 +5,10 @@ document.addEventListener('DOMContentLoaded', getRatings);
 let ratings = document.querySelectorAll('.number-rating');
 
 function getRatings() {
-    ratings.forEach((rating) => {
+    ratings.forEach((rating, index) => {
         let starPercentage = (rating.innerHTML / starsTotal * 100);
         const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
 
-        console.log(starPercentageRounded);
-
-        document.querySelector('.stars-inner').style.width = starPercentageRounded;
+        document.querySelector(`.stars-inner-${index + 1}`).style.width = starPercentageRounded;
     })
 }
