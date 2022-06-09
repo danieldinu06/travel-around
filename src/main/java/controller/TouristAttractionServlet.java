d6944b87561ae28e532da958f0f7c3fe39ede10c
freeDaTitty <danieldinu6091@gmail.com>
2022-06-09 16:11:34 +0300

    Inserted restaurants in Servlet

--- src/main/java/controller/TouristAttractionServlet.java
+++ src/main/java/controller/TouristAttractionServlet.java
@@ -1,6 +1,8 @@
 package controller;
 
 import config.TemplateEngineUtil;
+import model.Hotel;
+import model.Restaurant;
 import model.TouristAttraction;
 import model.User;
 import org.thymeleaf.TemplateEngine;
@@ -13,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/id/*"})
 public class TouristAttractionServlet extends HttpServlet {
@@ -26,6 +29,8 @@ public class TouristAttractionServlet extends HttpServlet {
         ApplicationService applicationService = ApplicationService.getInstance();
         Integer id = Integer.valueOf(request.getPathInfo().substring(1));
         TouristAttraction touristAttraction = applicationService.touristAttractionDao.get(id);
+        List<Hotel> hotels = applicationService.hotelDao.getHotelsByTouristAttraction(touristAttraction.getId());
+        List<Restaurant> restaurants = applicationService.restaurantDao.getAllByTouristAttraction(touristAttraction.getId());
 
         httpSession = request.getSession(true);
         User user = (User) httpSession.getAttribute("user");
@@ -34,6 +39,8 @@ public class TouristAttractionServlet extends HttpServlet {
             response.sendRedirect(request.getContextPath() + "/login");
 
         webContext.setVariable("touristAttraction", touristAttraction);
+        webContext.setVariable("hotels", hotels);
+        webContext.setVariable("restaurants", restaurants);
         if (user != null) {
             webContext.setVariable("username", user.getName());
         }
